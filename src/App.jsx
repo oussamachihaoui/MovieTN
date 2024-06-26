b9e2d8277f723a68ee366c34d3bb04b9e72c7f65
Oussama Chihaoui <chihaouioussama@gmail.com>
2024-06-26 16:19:18 +0100

    init project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className=' text-slate-950 text-3xl border border-red-600 m-10'>Hello world</h1>
+    </div>
+  )
+}
+
+export default App
